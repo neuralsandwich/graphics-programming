0be8ab8301b67d1d8dd28a96f13c2f26ce7072b2
Sean Jones <neuralsandwich@gmail.com>
2013-10-07 23:23:56 +0100

    Fixing dir layout: Moved Lesson 13 to src

---
+++ src/Lesson 13.cpp
@@ -0,0 +1,48 @@
+// Lesson 13.cpp : Defines the entry point for the console application.
+
+#include <render_framework\render_framework.h>
+#pragma comment (lib , "Render Framework" )
+
+using namespace render_framework;
+using namespace glm;
+
+int main()
+{
+	// Initialize the renderer
+	if (!renderer::get_instance().initialise()) return -1;
+
+	// Set the clear colour to cyan
+	glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+
+	/* Set the projection matrix */
+	// First get the aspect ratio (width/height)
+	float aspect = (float)renderer::get_instance().get_screen_width() /
+					(float)renderer::get_instance().get_screen_width();
+	// Use this to create projection matrix
+	auto projection = perspective(
+						degrees(quarter_pi<float>()),	// FOV
+						aspect,							// Aspect ratio
+						2.414f,							// Near plane
+						10000.0f);						// Far plane
+	// Set the projection matrix
+	renderer::get_instance().set_projection(projection);
+
+	// Set the view matrix
+	auto view = lookAt(
+					vec3(20.0f, 20.0f, 20.0f),	// Camera position
+					vec3(0.0f, 0.0f, 0.0f),		// Target
+					vec3(0.0f, 1.0f, 0.0f));	// Up vector
+	renderer::get_instance().set_view(view);
+
+	// Main render loop
+	while (renderer::get_instance().is_running())
+	{
+		// Check if running
+		if (renderer::get_instance().begin_render())
+		{
+			// End the render
+			renderer::get_instance().end_render();
+		}
+	}
+}
+
