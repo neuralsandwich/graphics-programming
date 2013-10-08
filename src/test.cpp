2a50d046997394a700cb8339ec2cc304d6b1bad3
Sean Jones <neuralsandwich@gmail.com>
2013-10-08 14:57:58 +0100

    Commiting updates to fix project

--- src/test.cpp
+++ src/test.cpp
@@ -1,50 +1,48 @@
-// Test.cpp
-// Sean Jones
+// Lesson 13.cpp : Defines the entry point for the console application.
 
-#include "../renderframework/render_framework.h"
-#include <glm/gtx/constants.hpp>
+#include <render_framework\render_framework.h>
+#pragma comment (lib , "Render Framework" )
 
-// For Debug
-#include <iostream>
-
-
-using namespace glm;
 using namespace render_framework;
+using namespace glm;
 
-
-int main(void)
+int main()
 {
-  // Initialise the renderer
-  if (!renderer::get_instance().initialise()) {
-    return -1;
-  }
+	// Initialize the renderer
+	if (!renderer::get_instance().initialise()) return -1;
 
-  // Set the clear colour to cyan (close to sky blue)
-  glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+	// Set the clear colour to cyan
+	glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
 
-  // Set the projection matrix
-  // First get the aspect ration (width/height)
-  float aspect = (float)renderer::get_instance().get_screen_width()
-              / (float)renderer::get_instance().get_screen_height();
-  // Use this to create the projection matrix
-  auto projection = perspective(degrees(quarter_pi<float>()), //FOV
-                                aspect,
-                                2.414f,
-                                10000.0f);
-  renderer::get_instance().set_view(projection);
+	/* Set the projection matrix */
+	// First get the aspect ratio (width/height)
+	float aspect = (float)renderer::get_instance().get_screen_width() /	
+					(float)renderer::get_instance().get_screen_width();
+	// Use this to create projection matrix
+	auto projection = perspective(
+						degrees(quarter_pi<float>()),	// FOV
+						aspect,							// Aspect ratio
+						2.414f,							// Near plane
+						10000.0f);						// Far plane
+	// Set the projection matrix
+	renderer::get_instance().set_projection(projection);
 
-  // Set the view matrix
-  auto view = lookAt(vec3(20.0f, 20.0f, 20.0f), // Camera position
-                     vec3(0.0f, 0.0f, 0.0f),    // Target
-                     vec3(0.0f, 1.0f, 0.0f));   // Up direction (orientation)
+	// Set the view matrix
+	auto view = lookAt(
+					vec3(20.0f, 20.0f, 20.0f),	// Camera position
+					vec3(0.0f, 0.0f, 0.0f),		// Target
+					vec3(0.0f, 1.0f, 0.0f));	// Up vector
+	renderer::get_instance().set_view(view);
 
-  // Main render loop
-  while (renderer::get_instance().is_running()) {
-    // Check if running
-    if (renderer::get_instance().begin_render()) {
-      //end the render
-      renderer::get_instance().end_render();
-    }
-  }
+	// Main render loop
+	while (renderer::get_instance().is_running())
+	{
+		// Check if running
+		if (renderer::get_instance().begin_render())
+		{
+			// End the render
+			renderer::get_instance().end_render();
+		}
+	}
+}
 
-} // main
